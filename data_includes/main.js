764ac002b9b4132bc779234c3172f97326ae4d7d
LU Yanru <39545418+Lu-Yanru@users.noreply.github.com>
2020-01-24 13:49:37 +0100

    fix typo

--- data_includes/main.js
+++ data_includes/main.js
@@ -188,7 +188,7 @@ newTimer(500)
       getCanvas("LDT2")
           .remove()
       ,
-      newText("pleasewait3","<p>End of the practice round. The actual experiment is about to begin.</p><p>Remember, press <b>F</b> to anser <b>No</b> and <b>J</b> to anser <b>Yes</b>.</p><p>You can press the spacebar to continue to the experiment now.</p>")
+      newText("pleasewait3","<p>End of the practice round. The actual experiment is about to begin.</p><p>Remember, press <b>F</b> to answer <b>No</b> and <b>J</b> to answer <b>Yes</b>.</p><p>You can press the spacebar to continue to the experiment now.</p>")
           .print()
       ,
       newKey(" ")
