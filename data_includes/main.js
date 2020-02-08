dc805e6f916c476dfac085a0cce867c477f3329a
LU Yanru <39545418+Lu-Yanru@users.noreply.github.com>
2020-02-08 19:45:29 +0100

    Integrated timer for sent2 and LDT
    Change the timer for sent2 in experiment from 1500 to 2000, because there are some longer (though filler) sentences that could take more time

--- data_includes/main.js
+++ data_includes/main.js
@@ -125,60 +125,58 @@ PennController( "welcome",
 )
 //.log( "ID" , getVar("ID") )
 
-// Practice Trail ////////
+// Practice Trial ////////
 
 PennController("practice",
       newTimer(500)
           .start()
           .wait()
       ,
+      newText("null", " ")
+      ,
       newText("Trial1", "John works all day. He is always exhausted.")
           .print()
       ,
-      //newText("pressspacebar", "<i><small>press spacebar to see next line<small></i>") //added this line to give additional instructions
-      //    .print()
-      //,
       newKey(" ")
           .wait()
       ,
       getText("Trial1")
           .remove()
       ,
-      //getText("pressspacebar")
-      //    .remove()
-      //,
       newText("Trial2", "John is a horse.")
           .print()
       ,
-      //getText("pressspacebar")
-      //    .print()
-      //,
-      newKey(" ")
+      newKey("space1", " ")
+          //.wait()
+      ,
+      newTimer(1500)
+          .start()
           .wait()
       ,
+      getKey("space1")
+          .test.pressed(" ")
+          .success(
       getText("Trial2")
           .remove()
       ,
-      //getText("pressspacebar") //This takes away the additional instruction
-      //    .remove()
-      //,
       newTimer("timer1", 1000)
           .start()
           .wait()
       ,
       newText("Trial3", "<b><font size='6'>farm</font></b>")
+          .settings.center()
+          .print()
       ,
       newText("No1", "<small>No [F]</small>")
+           .settings.center()
+           .settings.after(newText("Yes1", "<small>Yes [J]</small>").settings.css("padding-left", "100pt").settings.css("font-size", "medium"))
            .settings.css("font-size", "medium")
+           .print()
       ,
-      newText("Yes1", "<small>Yes [J]</small>")
-           .settings.css("font-size", "medium")
-      ,
-      //newText("nono","<i><small>If you think the letters don't form a word press F for No<small></i>") //Additional instruction for decision task text
+      //newText("Yes1", "<small>Yes [J]</small>")
+      //     .settings.css("font-size", "medium")
       //,
-      //newText("yesyes", "<i><small>If you do think it forms a word, press J for Yes<small></i>") //Yes, instruction text.
-      //,
-      newCanvas("LDT1", 700, 500)
+      /*newCanvas("LDT1", 700, 500)
           .settings.center()
           .settings.add("center at 50%", "top at 0%", getText("Trial3"))
           .settings.add(250, 50, getText("No1"))
@@ -186,16 +184,26 @@ PennController("practice",
           //.settings.add(200, 80, getText("nono")) //added an additional instruction
           //.settings.add(200, 100, getText("yesyes")) //Yes-text for additional instruction
           .print()
+      ,*/
+      newTimer("reminder1", 1500)
+          .settings.callback(getSelector("select1").select(getText("null")))
+          .settings.callback(getText("Trial3").settings.text("<small>Please be faster!</small>"))
+          .start()
       ,
-      newSelector()
+      newSelector("select1")
           .settings.add(getText("No1"), getText("Yes1"))
           .settings.keys("F", "J")
+          .settings.callback(getTimer("reminder1").stop())
+          .settings.callback(getText("Trial3").settings.text(" "))
           .wait()
       ,
-      getCanvas("LDT1")
+      /*getCanvas("LDT1")
+          .remove()
+      ,*/
+      getText("No1")
           .remove()
       ,
-      newText("proceed1", "<small>Press the space bar to read the next metaphor.</small>")
+      newText("proceed1", "<small>Press the space bar to read the next sentence.</small>")
           .print()
       ,
       newKey(" ")
@@ -204,74 +212,151 @@ PennController("practice",
       getText("proceed1")
           .remove()
       ,
+      getText("Trial3")
+          .remove()
+      )// what should happen if key successfully being pressed within the timer
+          .failure(
+            getText("Trial2")
+                .remove()
+            ,
+            newText("failure1", "<small>Too slow! Please try to read the sentences faster.</small>")
+                .print()
+            ,
+            newText("proceed2", "<small>Press the space bar to read the next sentence.</small>")
+                .print()
+            ,
+            newKey(" ")
+                .wait()
+            ,
+            getText("failure1")
+                .remove()
+            ,
+            getText("proceed2")
+                .remove()
+            ,
+            getText("Trial3")
+                .remove()
+          )//what should happen if key not being pressed within the timer
+      ,
+      newTimer(500)
+          .start()
+          .wait()
+      ,
       newText("Trial4", "Mary feeds her whole family. She is always giving them food.")
           .print()
       ,
-      //getText("pressspacebar")
-      //    .print()
-      //,
       newKey(" ")
           .wait()
       ,
       getText("Trial4")
           .remove()
       ,
-      //getText("pressspacebar")
-      //    .remove()
-      //,
       newText("Trial5", "Mary is a soup kitchen.")
           .print()
       ,
-      //getText("pressspacebar")
-      //    .print()
-      //,
-      newKey(" ")
-          .wait()
+      newKey("space2", " ")
+          //.wait()
       ,
-      getText("Trial5")
-          .remove()
-      ,
-      //getText("pressspacebar")
-      //    .remove()
-      //,
-      newTimer(400)
+      newTimer(1500)
           .start()
           .wait()
       ,
-      newText("Trial6", "<b><font size='6'>slint</font></b>")
+      getKey("space2")
+          .test.pressed(" ")
+          .success(
+            getText("Trial5")
+                .remove()
+            ,
+            newTimer(400)
+                .start()
+                .wait()
+            ,
+            newText("Trial6", "<b><font size='6'>slint</font></b>")
+                .settings.center()
+                .print()
+            ,
+            newText("No2", "<small>No [F]</small>")
+                 .settings.center()
+                 .settings.after(newText("Yes2", "<small>Yes [J]</small>").settings.css("padding-left", "100pt").settings.css("font-size", "medium"))
+                 .settings.css("font-size", "medium")
+                 .print()
+            ,
+            /*newText("Yes2", "<small>Yes [J]</small>")
+                 .settings.css("font-size", "medium")
+            ,
+            newCanvas("LDT2", 700, 500)
+                .settings.center()
+                .settings.add("center at 50%", "top at 0%", getText("Trial6"))
+                .settings.add(250, 50, getText("No2"))
+                .settings.add(410, 50, getText("Yes2"))
+                //.settings.add(200, 80, getText("nono")) //added an additional instruction
+                //.settings.add(200, 100, getText("yesyes")) //Yes-text for additional instruction
+                .print()
+            ,*/
+            newTimer("reminder2", 1500)
+                .settings.callback(getSelector("select2").select(getText("null")))
+                .settings.callback(getText("Trial6").settings.text("<small>Please be faster!</small>"))
+                .start()
+            ,
+            newSelector("select2")
+                .settings.add(getText("No2"), getText("Yes2"))
+                .settings.keys("F", "J")
+                .settings.callback(getTimer("reminder2").stop())
+                .settings.callback(getText("Trial6").settings.text(" "))
+                .wait()
+            ,
+            /*getCanvas("LDT2")
+                .remove()
+            ,*/
+            getText("No2")
+                .remove()
+            ,
+            newText("proceed3", "<small>Press the space bar to continue.</small>")
+                .print()
+            ,
+            newKey(" ")
+                .wait()
+            ,
+            getText("proceed3")
+                .remove()
+            ,
+            getText("Trial6")
+                .remove()
+          ) // what should happen if key being pressed within the timer
+          .failure(
+            getText("Trial5")
+                .remove()
+            ,
+            newText("failure2", "<small>Too slow! Please try to read the sentences faster.</small>")
+                .print()
+            ,
+            newText("proceed4", "<small>Press the space bar to continue.</small>")
+                .print()
+            ,
+            newKey(" ")
+                .wait()
+            ,
+            getText("failure2")
+                .remove()
+            ,
+            getText("proceed4")
+                .remove()
+            ,
+            getText("Trial6")
+                .remove()
+          ) // what should happen if key not being pressed within the timer
       ,
-      newText("No2", "<small>No [F]</small>")
-           .settings.css("font-size", "medium")
-      ,
-      newText("Yes2", "<small>Yes [J]</small>")
-           .settings.css("font-size", "medium")
-      ,
-      newCanvas("LDT2", 700, 500)
-          .settings.center()
-          .settings.add("center at 50%", "top at 0%", getText("Trial6"))
-          .settings.add(250, 50, getText("No2"))
-          .settings.add(410, 50, getText("Yes2"))
-          //.settings.add(200, 80, getText("nono")) //added an additional instruction
-          //.settings.add(200, 100, getText("yesyes")) //Yes-text for additional instruction
-          .print()
-      ,
-      newSelector()
-          .settings.add(getText("No1"), getText("Yes1"))
-          .settings.keys("F", "J")
-          .wait()
-      ,
-      getCanvas("LDT2")
-          .remove()
-      ,
-      newHtml("practice", "practice.html")
+      newHtml("practiceInfo", "practice.html")
           .print()
       ,
       newKey(" ")
           .wait()
       ,
-      getHtml("practice")
+      getHtml("practiceInfo")
           .remove()
 )
+
+
 
 // Main part of the experiment /////////
 
@@ -290,7 +375,9 @@ PennController.Template(
           .settings.log()
           .print()
       ,
-      newKey(" ")
+      newText("null", " ")
+      ,
+      newKey("sent1", " ")
           .settings.log()
           .wait()
       ,
@@ -306,53 +393,105 @@ PennController.Template(
           .settings.log()
           .print()
       ,
-      newKey(" ")
+      newKey("sent2", " ")
           .settings.log()
-          .wait()
+          //.wait()
       ,
-      getCanvas("canvas2")
-          .remove()
-      ,
-      newTimer("timer", variable.Time)
+      newTimer("tick", 2000)
           .start()
           .wait()
       ,
-      newText("Target", variable.Target)
-          .settings.log()
-          .settings.css("font-weight","bold")
-          .settings.css("font-size", "xx-large")
-      ,
-      newText("No", "<small>No [F]</small>")
-          .settings.css("font-size", "medium")
-      ,
-      newText("Yes", "<small>Yes [J]</small>")
-          .settings.css("font-size", "medium")
-      ,
-      newCanvas("LDT", 700, 500)
-          .settings.center()
-          .settings.add("center at 50%", "top at 0%", getText("Target"))
-          .settings.add(250, 50, getText("No"))
-          .settings.add(410, 50, getText("Yes"))
-          .settings.log()
-          .print()
-      ,
-      newSelector()
-          .settings.add(getText("No"), getText("Yes"))
-          .settings.keys("F", "J")
-          .settings.log()
-          .wait()
-      ,
-      getCanvas("LDT")
-          .remove()
-      ,
-      newText("proceed", "<small>Press the space bar to read the next metaphor.</small>")
-          .print()
-      ,
-      newKey(" ")
-          .wait()
-      ,
-      getText("proceed")
-          .remove()
+      getKey("sent2")
+          .test.pressed(" ")
+          .success(
+            getCanvas("canvas2")
+                .remove()
+            ,
+            getTimer("tick")
+                .stop()
+            ,
+            newTimer("timer", variable.Time)
+                .start()
+                .wait()
+            ,
+            newText("Target", variable.Target)
+                .settings.center()
+                .settings.log()
+                .settings.css("font-weight","bold")
+                .settings.css("font-size", "xx-large")
+                .print()
+            ,
+            newText("No", "<small>No [F]</small>")
+                 .settings.center()
+                 .settings.after(newText("Yes", "<small>Yes [J]</small>").settings.css("padding-left", "100pt").settings.css("font-size", "medium"))
+                 .settings.css("font-size", "medium")
+                 .print()
+            ,
+            /*newText("Yes", "<small>Yes [J]</small>")
+                .settings.css("font-size", "medium")
+            ,
+            newCanvas("LDT", 700, 500)
+                .settings.center()
+                .settings.add("center at 50%", "top at 0%", getText("Target"))
+                .settings.add(250, 50, getText("No"))
+                .settings.add(410, 50, getText("Yes"))
+                .settings.log()
+                .print()
+            ,*/
+            newTimer("reminder", 1500)
+                .settings.callback(getSelector("select").select(getText("null")))
+                .settings.callback(getText("Target").settings.text("<small>Please be faster!</small>").settings.css("font-weight","normal").settings.css("font-size", "x-large"))
+                .start()
+            ,
+            newSelector("select")
+                .settings.add(getText("No"), getText("Yes"))
+                .settings.keys("F", "J")
+                .settings.callback(getTimer("reminder").stop())
+                .settings.callback(getText("Target").settings.text(" "))
+                .settings.log()
+                .wait()
+            ,
+            getText("No")
+                .remove()
+            ,
+            newText("proceed", "<small>Press the space bar to read the next sentence.</small>")
+                .print()
+            ,
+            newKey(" ")
+                .wait()
+            ,
+            getText("proceed")
+                .remove()
+            ,
+            getText("Target")
+                .remove()
+            /*getCanvas("LDT")
+                .remove()
+            ,*/
+          )
+          .failure(
+            getCanvas("canvas2")
+                .remove()
+            ,
+            newText("failure", "<small>Too slow! Please try to read the sentences faster.<small>")
+                .print()
+            ,
+            newText("proceedEx", "<small>Press the space bar to read the next sentence.<small>")
+                .print()
+            ,
+            newKey(" ")
+                .wait()
+            ,
+            getText("failure")
+                .remove()
+            ,
+            getText("proceedEx")
+                .remove()
+            ,
+            getText("Target")
+                .remove()
+          )
+
   )
   //.log( "ID"     , getVar("ID")    )
   .log( "ItemNum"   ,variable.ItemNum   )
